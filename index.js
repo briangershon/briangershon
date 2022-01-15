7a688de47ab546ed5bf62c45cc0f5e2fde130af7
Brian Gershon <briangershon@users.noreply.github.com>
2022-01-15 14:31:09 -0800

    Automatically update README from template via GH Action.

---
+++ index.js
@@ -0,0 +1,49 @@
+const readme = `\
+## Recent Blog Posts
+
+<table>
+  <tr>
+    <td width="40%">
+      <img src="https://www.briangershon.com/blog/exploring-smart-contracts/slice.jpeg" />
+    </td>
+    <td width="60%"><h3>Exploring Smart Contracts</h3><p>Build a new generation of apps based on the blockchain and Smart Contracts. Fund your favorite creator (which might be you!), build games with real economies, run your organization with distributed ownership and governance models.</p><a href="https://www.briangershon.com/blog/exploring-smart-contracts/">Read it!</a></td>
+   </tr>
+  <tr>
+    <td width="40%">
+      <img src="https://www.briangershon.com/blog/developing-real-time-games-with-cloudflare-durable-objects-and-websockets/slice.jpeg" />
+    </td>
+    <td width="60%"><h3>Developing Real-time Games with Cloudflare Durable Objects and Websockets</h3><p>What would a scalable game server look like on serverless? I kicked the Cloudflare tires and built an open source Planning Poker app to find out.</p><a href="https://www.briangershon.com/blog/developing-real-time-games-with-cloudflare-durable-objects-and-websockets/">Read it!</a></td>
+   </tr>
+   <tr>
+    <td width="40%">&nbsp;</td><td width="60%">Read more at <a href="https://www.briangershon.com/blog/">briangershon/blog</a></td>
+   </tr>
+</table>
+
+## I enjoy experimenting ☀️
+
+- My project starter templates demonstrating best practices for a variety of technologies:
+  - Web3: [OpenZeppelin Solidity Hardhat Playground](https://github.com/briangershon/openzeppelin-solidity-hardhat-playground)
+  - JavaScript and TypeScript: [VanillaJS + TypeScript + Webpack](https://github.com/briangershon/typescript-webpack-starter) | [React.js + Snowpack](https://github.com/briangershon/react-minimal) | [VanillaJS](https://github.com/briangershon/vanilla-js-minimal) | [Next.js](https://github.com/briangershon/nextjs-starter) | [three.js](https://github.com/briangershon/threejs-minimal) | [Eleventy](https://github.com/briangershon/eleventy-minimal) | [Glitch](https://github.com/briangershon/glitch-minimal) | [Node.js](https://github.com/briangershon/nodejs-minimal)
+  - NPM Package: [Github-hosted NPM Package](https://github.com/briangershon/npm-package-minimal)
+  - Python: [Setup Python for Machine Learning](https://github.com/briangershon/setup-python-for-machine-learning)
+  - Go: [Setup Go](https://github.com/briangershon/setup-go)
+  - Java: [Minecraft Plugin](https://github.com/briangershon/minecraft-plugin)
+
+<!-- - running Serverless. My growing list of [website bookmarks](https://www.briangershon.com/bookmarks/) is a serverless service which accepts URLs and then generates metadata and primary images for those sites. Bookmarks are rendered on my [Eleventy-based](https://www.briangershon.com/blog/choose-your-own-adventure-with-eleventy/) blog.
+
+- writing my own do-it-yourself animations -- [UFOs](https://briangershon.github.io/ufo/) | [Animated billboard](https://marbles.briangershon.com/)
+
+- toying with Slack bots -- [FaunaDB](https://github.com/briangershon/team-rotation-faunadb) persistence (for a Slackbot project) -->
+
+- dabbling with the Internet of Things -- [IoT and Arduino Cloud](https://www.briangershon.com/blog/arduino-iot-explore-kit-getting-started-air-quality-sunrise-sunset/)
+
+<!-- - creating tools and Github Actions -- used in my starter templates (mentioned above) and nice for running [cron jobs](https://github.com/briangershon/github-actions-cron) too -->
+
+- going on an algorithm kick on [CodeWars](https://www.codewars.com/users/briangershon), my [JavaScript algorithms repo](https://github.com/briangershon/algorithms-in-javascript) and [Advent of Code](https://adventofcode.com/)
+
+---
+
+📫 You can find me in various communities by visiting [briangershon.com/about](https://www.briangershon.com/about/)
+`
+
+console.log(readme)
