ff746954833fcdc95576afadc7d19ccd814ee652
Brian Gershon <briangershon@users.noreply.github.com>
2022-01-15 19:00:34 -0800

    Dynamically generate latest blog posts from RSS feed (#2)
    - dynamically generate blog entries via RSS feed
    - add remaining static content via separate file

--- index.js
+++ index.js
@@ -1,48 +1,67 @@
-const readme = `\
+/*
+  Generate README.md
+  Author: Brian Gershon
+
+  - Add most recent blog entries based on my blog RSS feed
+  - Add static content
+
+*/
+const fs = require("fs").promises;
+const axios = require("axios");
+const cheerio = require("cheerio");
+
+const RSS_FEED_URL = "https://www.briangershon.com/feed.rss";
+
+async function generateBlog() {
+  const response = await axios.get(RSS_FEED_URL);
+
+  const $ = cheerio.load(response.data, {
+    xmlMode: true,
+  });
+
+  let result = [];
+
+  $("item").each(function (i, elm) {
+    const title = $(elm).find("title").text();
+    const link = $(elm).find("link").text();
+    const description = $(elm).find("description").text();
+
+    result.push(`\
+  <tr>
+    <td width="40%">
+      <img src="${link}slice.jpeg" />
+    </td>
+    <td width="60%">
+      <h3>${title}</h3>
+      <p>${description}</p>
+      <a href="${link}">Read it!</a>
+    </td>
+  </tr>
+`);
+  });
+
+  // return result with last n recent blog posts
+  return `\
 ## Recent Blog Posts
 
 <table>
-  <tr>
-    <td width="40%">
-      <img src="https://www.briangershon.com/blog/exploring-smart-contracts/slice.jpeg" />
-    </td>
-    <td width="60%"><h3>Exploring Smart Contracts</h3><p>Build a new generation of apps based on the blockchain and Smart Contracts. Fund your favorite creator (which might be you!), build games with real economies, run your organization with distributed ownership and governance models.</p><a href="https://www.briangershon.com/blog/exploring-smart-contracts/">Read it!</a></td>
-   </tr>
-  <tr>
-    <td width="40%">
-      <img src="https://www.briangershon.com/blog/developing-real-time-games-with-cloudflare-durable-objects-and-websockets/slice.jpeg" />
-    </td>
-    <td width="60%"><h3>Developing Real-time Games with Cloudflare Durable Objects and Websockets</h3><p>What would a scalable game server look like on serverless? I kicked the Cloudflare tires and built an open source Planning Poker app to find out.</p><a href="https://www.briangershon.com/blog/developing-real-time-games-with-cloudflare-durable-objects-and-websockets/">Read it!</a></td>
-   </tr>
-   <tr>
-    <td width="40%">&nbsp;</td><td width="60%">Read more at <a href="https://www.briangershon.com/blog/">briangershon/blog</a></td>
-   </tr>
-</table>
+${result.slice(-2).reverse().join("")}
+</table>`;
+}
 
-## I enjoy experimenting ☀️
+async function generateStatic() {
+  return fs.readFile("./readme.static.md", {
+    encoding: "utf8",
+    flag: "r",
+  });
+}
 
-- My project starter templates demonstrating best practices for a variety of technologies:
-  - Web3: [OpenZeppelin Solidity Hardhat Playground](https://github.com/briangershon/openzeppelin-solidity-hardhat-playground)
-  - JavaScript and TypeScript: [VanillaJS + TypeScript + Webpack](https://github.com/briangershon/typescript-webpack-starter) | [React.js + Snowpack](https://github.com/briangershon/react-minimal) | [VanillaJS](https://github.com/briangershon/vanilla-js-minimal) | [Next.js](https://github.com/briangershon/nextjs-starter) | [three.js](https://github.com/briangershon/threejs-minimal) | [Eleventy](https://github.com/briangershon/eleventy-minimal) | [Glitch](https://github.com/briangershon/glitch-minimal) | [Node.js](https://github.com/briangershon/nodejs-minimal)
-  - NPM Package: [Github-hosted NPM Package](https://github.com/briangershon/npm-package-minimal)
-  - Python: [Setup Python for Machine Learning](https://github.com/briangershon/setup-python-for-machine-learning)
-  - Go: [Setup Go](https://github.com/briangershon/setup-go)
-  - Java: [Minecraft Plugin](https://github.com/briangershon/minecraft-plugin)
+async function generatePage() {
+  console.log(await generateBlog());
+  console.log();
+  console.log(await generateStatic());
+}
 
-<!-- - running Serverless. My growing list of [website bookmarks](https://www.briangershon.com/bookmarks/) is a serverless service which accepts URLs and then generates metadata and primary images for those sites. Bookmarks are rendered on my [Eleventy-based](https://www.briangershon.com/blog/choose-your-own-adventure-with-eleventy/) blog.
-
-- writing my own do-it-yourself animations -- [UFOs](https://briangershon.github.io/ufo/) | [Animated billboard](https://marbles.briangershon.com/)
-
-- toying with Slack bots -- [FaunaDB](https://github.com/briangershon/team-rotation-faunadb) persistence (for a Slackbot project) -->
-
-- dabbling with the Internet of Things -- [IoT and Arduino Cloud](https://www.briangershon.com/blog/arduino-iot-explore-kit-getting-started-air-quality-sunrise-sunset/)
-
-<!-- - creating tools and Github Actions -- used in my starter templates (mentioned above) and nice for running [cron jobs](https://github.com/briangershon/github-actions-cron) too -->
-
-- going on an algorithm kick on [CodeWars](https://www.codewars.com/users/briangershon), my [JavaScript algorithms repo](https://github.com/briangershon/algorithms-in-javascript) and [Advent of Code](https://adventofcode.com/)
-
----
-
-📫 You can find me in various communities by visiting [briangershon.com/about](https://www.briangershon.com/about/)`;
-
-console.log(readme);
+generatePage().then(() => {
+  process.exit();
+});
